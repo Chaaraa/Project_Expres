52abfb5b33b65979398d71a8a9ea93c1cddcefb7
Lina155 <AlinfDEmaGitHub2021>
2021-03-11 16:31:26 +0200

    pot

--- index.js
+++ index.js
@@ -1,33 +1,48 @@
 const express = require('express')
 
-const PORT = process.env.PORT ?? 5050
+const cors = require ('cors')
+const PORT = 5050
 const app = express()
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
+app.use(function (req,res,next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type,origin,accept');
+    next();
+})
 const vinnytsia = [
     {
-        temp: 100,
-        hund: 6,
-        sunrise: 6000,
+        "temp":271.04,
+        "humidity":59,
+        "timezone":7200,
+        "name":"Vinnytsia",
     }
 ]
 const london = [
     {
-        temp: 100,
-        hund: 63,
-        sunrise: 5000,
+
+        "temp": 280.32,
+        "humidity": 81,
+        "timezone":0,
+        "name": "London",
+
     }
 ]
 const paris = [
     {
-        temp: 100,
-        hund: 62,
-        sunrise: 7000,
+        "temp":286.05,
+        "humidity":76,
+        "timezone":3600,
+        "name":"Paris",
+
     }
 ]
-app.get('/vinnytsia',(req,res)=>res.json(vinnytsia))
-app.get('/london',(req,res)=>res.json(london))
-app.get('/paris',(req,res)=>res.json(paris))
+app.get('/Vinnytsia',(req,res)=>res.json(vinnytsia))
+app.get('/London',(req,res)=>res.json(london))
+app.get('/Paris',(req,res)=>res.json(paris))
+
+
 
 app.listen(PORT, () => {
     console.log (`Server can been started on port ${PORT}...`)
